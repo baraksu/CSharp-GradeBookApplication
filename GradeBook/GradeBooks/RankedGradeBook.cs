4d5b45c6de56ba6408f843d3ebcaf32a2ecd069f
baraksu <baraksu@gmail.com>
2018-09-08 23:54:13 +0300

    Finished module 2 A

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using GradeBook.Enums;
 namespace GradeBook.GradeBooks
 {
@@ -8,11 +9,25 @@ namespace GradeBook.GradeBooks
         {
             Type=GradeBookType.Ranked;        
         }
-        public override char GetLetterGrade  (double averageGrade)
+       /// public\soverride\schar\sGetLetterGrade\s?[(]\s?double\saverageGrade\s?[)])"
+        public override char GetLetterGrade (double averageGrade)
         {
-        if (Students.Count<5) 
+        if (Students.Count<5){
             throw new InvalidOperationException();
-         return 'A';
+        } 
+           
+           var thershold = (int)Math.Ceiling(Students.Count * 0.2);
+           var grades = Students.OrderByDescending (e =>e.AverageGrade).Select (e =>e.AverageGrade).ToList();
+           if (grades[thershold -1]<= averageGrade)
+           return 'A';
+           else if (grades [thershold * 2 -1] <= averageGrade)
+           return 'B';
+           else if (grades [thershold * 3 -1] <= averageGrade)
+           return 'C';
+           else if (grades [thershold * 4 -1] <= averageGrade)
+           return 'D';
+           else return 'F';
+
         } 
     }   
 }    
