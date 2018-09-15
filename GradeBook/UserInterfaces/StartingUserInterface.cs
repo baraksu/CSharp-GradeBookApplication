ffaf47cb93f797c17495380c3051a1e142ad5a15
baraksu <baraksu@gmail.com>
2018-09-15 20:25:44 +0300

    Completed the module2

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -43,7 +43,7 @@ namespace GradeBook.UserInterfaces
             BaseGradeBook gradeBook; 
             if (type == "standard")       
              gradeBook = new StandardGradeBook(name);
-             else if (type == "Ranked")
+             else if (type == "ranked")
               gradeBook = new RankedGradeBook(name);
              else
               {
